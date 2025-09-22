73ddeb7b1902982ac6cafaaa94df1ef6258bc52a
Jenny <jennyliu07@gmail.com>
2025-09-22 21:02:49 +0800

    added supportedAccountLinks

--- src/app/provider/para-provider.tsx
+++ src/app/provider/para-provider.tsx
@@ -50,6 +50,7 @@ export function ParaProvider({
         authLayout: ["AUTH:FULL", "EXTERNAL:FULL"],
         oAuthMethods: ["GOOGLE", "TWITTER"],
         onRampTestMode: true,
+        supportedAccountLinks: ["EMAIL", "TWITTER", "GOOGLE"],
         theme: {
           foregroundColor: "#010101",
           backgroundColor: "#F8F8F8",
